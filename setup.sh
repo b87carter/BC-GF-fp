f0a106e5711ac62159d5a62bde58f4fe353c0b6c
b87carter <56475172+b87carter@users.noreply.github.com>
2020-12-08 22:27:33 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-mkdir -p datasets
+# mkdir -p datasets
 mkdir -p glove
 
-curl https://s3.us-east-2.amazonaws.com/mrqa/release/v2/train/SQuAD.jsonl.gz -o datasets/squad_train.jsonl.gz
-curl https://s3.us-east-2.amazonaws.com/mrqa/release/v2/train/NewsQA.jsonl.gz -o datasets/newsqa_train.jsonl.gz
-curl https://s3.us-east-2.amazonaws.com/mrqa/release/v2/dev/SQuAD.jsonl.gz -o datasets/squad_dev.jsonl.gz
-curl https://s3.us-east-2.amazonaws.com/mrqa/release/v2/dev/NewsQA.jsonl.gz -o datasets/newsqa_dev.jsonl.gz
-curl http://participants-area.bioasq.org/MRQA2019/ -o datasets/bioasq.jsonl.gz
-curl http://cs.utexas.edu/~gdurrett/courses/fa2020/squad_dev_addOneSent_mrqa.json.gz -o datasets/squad_adversarial_addonesent.jsonl.gz
+# curl https://s3.us-east-2.amazonaws.com/mrqa/release/v2/train/SQuAD.jsonl.gz -o datasets/squad_train.jsonl.gz
+# curl https://s3.us-east-2.amazonaws.com/mrqa/release/v2/train/NewsQA.jsonl.gz -o datasets/newsqa_train.jsonl.gz
+# curl https://s3.us-east-2.amazonaws.com/mrqa/release/v2/dev/SQuAD.jsonl.gz -o datasets/squad_dev.jsonl.gz
+# curl https://s3.us-east-2.amazonaws.com/mrqa/release/v2/dev/NewsQA.jsonl.gz -o datasets/newsqa_dev.jsonl.gz
+# curl http://participants-area.bioasq.org/MRQA2019/ -o datasets/bioasq.jsonl.gz
+# curl http://cs.utexas.edu/~gdurrett/courses/fa2020/squad_dev_addOneSent_mrqa.json.gz -o datasets/squad_adversarial_addonesent.jsonl.gz
 
-# curl http://nlp.stanford.edu/data/wordvecs/glove.6B.zip
-# unzip glove.6B.zip
-# mv glove.6B.*.txt glove/
-# rm glove.6B.zip
+curl http://nlp.stanford.edu/data/wordvecs/glove.6B.zip
+unzip glove.6B.zip
+mv glove.6B.*.txt glove/
+rm glove.6B.zip
 
 echo
 echo "done!"
